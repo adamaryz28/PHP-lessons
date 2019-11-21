f1e7da0c36c2b5581510e1ea30c21475c1198155
adamaryz28 <58046771+adamaryz28@users.noreply.github.com>
2019-11-21 11:16:57 -0800

    Create calcForm.php

---
+++ calcForm.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<h1> Hello World!</h1>
+		
+		<form action = "runCalc.php" method ="get">
+		Num1 <input type ="text" name="num1"><br>
+		Num2 <input type="text" name="num2"><br>
+		<select name = "type">
+		<option>Add</option>
+		<option>Subtract</option>
+		</select>
+		<input type ="submit" value="Calculate">
+		</form>
+	</body>
+</html>
