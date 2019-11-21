bed0969ad9a40fe0d98ea0afba404bce8fbc6c56
adamaryz28 <58046771+adamaryz28@users.noreply.github.com>
2019-11-21 10:18:34 -0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<h1> Hello World!</h1>
+		<?php 
+			echo "hi its me";
+			$myVariable = "<h2>hi its me again</h2>";
+			echo $myVariable;
+		?>
+	</body>
+</html>
