f91e5c24e58a055f054f6a692c6ad1229718d366
adamaryz28 <58046771+adamaryz28@users.noreply.github.com>
2019-11-22 10:38:19 -0800

    Create superGlobals.php

---
+++ superGlobals.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+	<style>
+	#peeps {
+		color: red;
+	}
+	</style>
+	</head>
+	<body>
+		<?php
+			echo "Hello World - from:".$_SERVER['SERVER_SOFTWARE'];
+			echo "<h1 id = 'peeps'>".$_SERVER['HTTP_HOST']."</h1>";
+		?>
+	</body>
+</html>
