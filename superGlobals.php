2f323274812f10a649010a7fbb5cd40d7f44d5c9
adamaryz28 <58046771+adamaryz28@users.noreply.github.com>
2019-11-22 10:48:48 -0800

    Update superGlobals.php

--- superGlobals.php
+++ superGlobals.php
@@ -10,6 +10,11 @@
 		<?php
 			echo "Hello World - from:".$_SERVER['SERVER_SOFTWARE'];
 			echo "<h1 id = 'peeps'>".$_SERVER['HTTP_HOST']."</h1>";
+			
+			$testArray = array('test1', 'test2');
+			foreach($testArray as $test){
+				echo $test;
+			}
 		?>
 	</body>
-</html>
+</html>>
